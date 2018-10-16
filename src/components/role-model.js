ef3d35d8bdeb6335b81e10d6156e7d266948dd65
Sarah Mamy <mamysrh@gmail.com>
2018-10-16 23:07:43 +0200

    Draggable Back

--- src/components/role-model.js
+++ src/components/role-model.js
@@ -6,7 +6,7 @@ const RoleModel = ({ name, comment, url, imageUrl}) => (
       <h3 className='pt30'><a href={url} target='_blank'>{name}</a></h3>
       <p className='pt18 pb6'>{comment}</p>
       <div className='z-neg1 flex-parent flex-parent--center-main flex-parent--end-cross mt12 px12 bg-gradient h240'>
-        <div className='flex-child hmax180 wmax300 shadow-darken25-bold'><img src={imageUrl} className=''/></div>
+        <div className='flex-child hmax180 wmax300 shadow-darken25-bold'><img draggable={false} src={imageUrl} className=''/></div>
       </div>
       <div className='z5 bg-white h36'></div>
     </div>
